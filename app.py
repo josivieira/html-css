1231304301783e7d284fbb6cdd38c6f7ea142555
josivieira <v.josiane@escola.pr.gov.br>
2025-06-12 08:35:28 -0300

    nv

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import  Flask
+from routes.main_routes import main_bp
+from routes.usuario_routes import user_bp
+
+
+app = Flask(__name__)
+app.secret_key = 'jhow'
+
+# registrar os bluerprints
+app.register_blueprint(main_bp)
+app.register_blueprint(user_bp, url_prefix='/usuario')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
